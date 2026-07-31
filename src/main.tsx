4a5a50925e0dfd60daa77d6e78b9efc60c5a533a
Mackson Victor <macksongaspar@gmail.com>
2026-07-31 13:49:40 -0300

    Carrega acabamento das DMs V4

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import "./styles.css";
 import "./access-control.css";
 import "./workspace-polish.css";
 import "./direct-messages.css";
+import "./direct-messages-v4.css";
 import "./workspace-layout-fix.css";
 import "./workspace-brand.css";
 import "./experience-polish.css";
