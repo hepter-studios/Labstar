9cfc5d3e9b79e162dbfb29a236a3631ca32d4f8a
Mackson Victor <macksongaspar@gmail.com>
2026-07-31 12:30:29 -0300

    Ativa central de mensagens diretas v3

--- src/components/CollaborationHub.tsx
+++ src/components/CollaborationHub.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import type { Member } from "../lib/supabase";
-import { DirectMessagesHub } from "./DirectMessagesHubV2";
+import { DirectMessagesHub } from "./DirectMessagesHubV3";
 import { CollaborationHub as LegacyCollaborationHub } from "./LegacyCollaborationHub";
 
 type CollaborationHubProps = {
