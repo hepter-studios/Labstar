b974d3256096de645d5973710506a864a1098dc7
Mackson Victor <macksongaspar@gmail.com>
2026-08-05 15:28:25 -0300

    Carrega correções finais de viewport e mobile

--- src/main.tsx
+++ src/main.tsx
@@ -59,6 +59,7 @@ import "./dashboard-work-surface.css";
 import "./message-work-item.css";
 import "./direct-messages-v6.css";
 import "./direct-messages-v7.css";
+import "./mobile-responsive.css";
 
 const BRAND_INTRO_DURATION_MS = 2350;
 const NATIVE_BRIDGE_TIMEOUT_MS = 4000;
