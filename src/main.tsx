f75cf76337d4f00bc153e260f00b9290834942f1
Mackson Victor <macksongaspar@gmail.com>
2026-08-05 21:18:44 -0300

    Carrega correção final da altura da interface

--- src/main.tsx
+++ src/main.tsx
@@ -61,6 +61,7 @@ import "./direct-messages-v6.css";
 import "./direct-messages-v7.css";
 import "./mobile-responsive.css";
 import "./private-messages-final.css";
+import "./viewport-layout-hotfix.css";
 
 const BRAND_INTRO_DURATION_MS = 2350;
 const NATIVE_BRIDGE_TIMEOUT_MS = 4000;
